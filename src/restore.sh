6f4e0202c4a4070f069a06dcab4423abf611fd82
crakoucas <crakoucas@gmail.com>
2018-02-07 00:25:26 +0100

    restore ok

--- src/restore.sh
+++ src/restore.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-FILE=${FILE}
 MONGO_HOST=${MONGO_HOST:-mongo}
 MONGO_PORT=${MONGO_PORT:-27017}
 
+read -p 'Nom de la save sur ftp/backup : ' FILE
 echo "FTP get"
-ncftpget  -u ${FTP_USER} -p ${FTP_PASSWORD} ${FTP_HOST} /${FTP_FOLDER}/$FILE
-echo "FTP get ok"
+ncftpget  -u ${FTP_USER} -p ${FTP_PASSWORD} ftp://${FTP_HOST}/${FTP_FOLDER}/$FILE
 echo "Tar file"
 tar zxvf $FILE
 echo "Mongorestore"
-mongorestore -h $MONGO_HOST -p $MONGO_PORT /dump
+mongorestore -h $MONGO_HOST -p $MONGO_PORT dump/
 echo "Mongorestore OK"
-rm -rf /dump
+rm -rf dump/
 rm $FILE
 echo "File delete"
